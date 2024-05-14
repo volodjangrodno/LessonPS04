1d71bb4a6f29de069cd9be344b4e4e5b860bdee1
volodjangrodno <parthnersclub.ru@gmail.com>
2024-05-15 00:58:40 +0300

    new cod in main + images

--- main.py
+++ main.py
@@ -3,6 +3,11 @@ import time
 
 browser = webdriver.Chrome()
 
-browser.get("https://www.google.com/")
-time.sleep(10)
-browser.quit()
+browser.get("https://en.wikipedia.org/wiki/Document_Object_Model")
+browser.save_screenshot("dom.png")
+#В кавычках указываем название, которое присвоится скриншоту
+time.sleep(5)
+browser.get("https://ru.wikipedia.org/wiki/Selenium")
+browser.save_screenshot("selenium.png")
+time.sleep(3)
+browser.refresh()  # Перезагрузка страницы
